4d9d3394e9d86e2e33f5a05e6dc8df0c091449c0
arif <arif@bitmascot.com>
2021-03-26 16:12:03 +1100

    Base

--- fc.py
+++ fc.py
@@ -1,14 +1,40 @@
 from input_layer import InputLayer
+from layer_dense import LayerDense
+from optimizer_adam import OptimizerAdam
 from output_layer import OutputLayer
 import numpy as np
 
 class FullyConnected:
-    def __init__(self, n_input, n_output):
-        self.input_layer = InputLayer(n_input)
-        self.output_layer = OutputLayer(n_output, self.input_layer)
+    def __init__(self, train_x, train_y, test_x, test_y):
+        self.input_layer = InputLayer(train_x.shape[1])
+        self.output_layer = OutputLayer(len(train_y), self.input_layer)
         self.input_layer.prev_layer = self.output_layer
         self.accuracy = 0
         self.loss = 0
+        self.train_x = train_x
+        self.train_y = train_y
+        self.test_x = test_x
+        self.test_y = test_y
+        self.preprocess()
+        self.add_layer(LayerDense(train_x.shape[1], 64))
+        self.add_layer(LayerDense(32,64))
+        self.add_layer(LayerDense(64,len(train_y)))
+        self.optimizer = OptimizerAdam(learning_rate=0.05, decay=5e-7)
+
+    def preprocess(self):
+        self.train_x = (self.train_x.astype(np.float32) - 127.5) / 127.5
+        self.test_x = (self.test_x.astype(np.float32) - 127.5) / 127.5
+
+        self.train_x = self.train_x.reshape(self.train_x.shape[0], -1)
+        self.test_x = self.test_x.reshape(self.test_x.shape[0], -1)
+
+        keys = np.array(range(self.train_x.shape[0]))
+        np.random.shuffle(keys)
+        self.train_x = self.train_x[keys]
+
+        keys = np.array(range(self.test_x.shape[0]))
+        np.random.shuffle(keys)
+        self.test_x = self.test_x[keys]
 
     def add_layer(self, layer):
         source_layer = self.output_layer.prev_layer
@@ -48,3 +74,15 @@ class FullyConnected:
         while layer is not None:
             print(f"{layer.n_neurons} - {type(layer).__name__}")
             layer = layer.next_layer
+
+    def train(self):
+        for epoch in range(1):
+            self.forward_backward(self.train_x, self.train_y)
+            self.optimizer.optimise(self)
+
+            if not epoch % 100:
+                print(f'epoch: {epoch} , ' +
+                      f'acc: {self.accuracy:.3f} , ' +
+                      f'loss: {self.loss:.3f} , ' +
+                      f'lr: {self.optimizer.current_learning_rate:.3f} ')
+
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 import numpy as np
 import image_viewer
+from fc import FullyConnected
 from mnist import Mnist
 
 mnist = Mnist()
 train_x,train_y = mnist.get_train_data()
 test_x,test_y = mnist.get_test_data()
 
-train_x = (train_x.astype(np.float32) - 127.5 ) / 127.5
-test_x = (test_x.astype(np.float32) - 127.5 ) / 127.5
-
-train_x = train_x.reshape(train_x.shape[0], -1)
-test_x = test_x.reshape(test_x.shape[0], -1)
-
-keys = np.array(range(train_x.shape[0]))
-np.random.shuffle(keys)
-train_x = train_x[keys]
-
-keys = np.array(range(test_x.shape[0]))
-np.random.shuffle(keys)
-test_x = test_x[keys]
-
-image_viewer.view_image(train_x[0])
+fc = FullyConnected(train_x, train_y, test_x, test_y)
+fc.train()
 
