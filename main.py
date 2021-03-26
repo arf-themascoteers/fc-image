b470c264c5a47b7b61574c19f35615fa14dae87a
arif <arif@bitmascot.com>
2021-03-26 15:08:16 +1100

    Base

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
-import nnfs
-from nnfs.datasets import spiral_data
-import matplotlib.pyplot as plt
-import numpy as np
+from zipfile import ZipFile
+import os
+import urllib
+import urllib.request
 
-from activation_relu import ActivationReLU
-from fc import FullyConnected
-from layer_dense import LayerDense
-from optimizer_adam import OptimizerAdam
+URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
+FILE = 'fashion_mnist_images.zip'
+FOLDER = 'data'
+CHECK_FILE = "data/train"
 
-nnfs.init()
+if not os.path.exists(CHECK_FILE):
+    urllib.request.urlretrieve(URL, FILE)
+    with ZipFile(FILE) as zip_images:
+        zip_images.extractall(FOLDER)
 
-X, y = spiral_data( samples = 100 , classes = 3 )
-
-fc = FullyConnected(2,3)
-fc.add_layer(LayerDense( 2 , 64 ))
-fc.add_layer(ActivationReLU(64))
-fc.add_layer(LayerDense( 64 , 3 ))
-optimizer = OptimizerAdam( learning_rate = 0.05 , decay = 5e-7 )
-
-for epoch in range ( 10001 ):
-    fc.forward_backward(X,y)
-    optimizer.optimise(fc)
-
-    if not epoch % 100:
-        print(f'epoch: {epoch} , ' +
-        f'acc: {fc.accuracy:.3f} , ' +
-        f'loss: {fc.loss:.3f} , ' +
-        f'lr: {optimizer.current_learning_rate:.3f} ' )
-
-plt.scatter(X[:, 0 ], X[:, 1 ], c = y, s = 40 , cmap = 'brg', label = 'initial' )
-plt.show()
-
-y = np.argmax(fc.output_layer.output, axis = 1)
-
-plt.scatter(X[:, 0 ], X[:, 1 ], c = y, s = 40 , cmap = 'brg',label = 'predicted', alpha=0.3 )
-plt.show()
+labels = os.listdir( 'data/train' )
